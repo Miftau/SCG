b1b6c84a8a0afa45c206c70808bbf5788aae38e3
Your Name <john@arabgiftcard.com>
2025-07-16 16:32:15 +0300

    add

--- app/components/DataCenter/ServerLocation/ServerLocation.jsx
+++ app/components/DataCenter/ServerLocation/ServerLocation.jsx
@@ -6,67 +6,67 @@ const ServerLocation = () => {
   const locations = [
     {
       name: "أمريكا",
-      flag: "/images/Amreci.png",
+      flag: "/images/flag/Amreci.png",
       response: "20ms",
       color: "#46CC3F",
     },
     {
       name: "المانيا",
-      flag: "/images/Germen.png",
+      flag: "/images/flag/Germen.png",
       response: "35ms",
       color: "#46CC3F",
     },
     {
       name: "تركيا",
-      flag: "/images/turkey.png",
+      flag: "/images/flag/turkey.png",
       response: "50ms",
       color: "#46CC3F",
     },
     {
       name: "ايطاليا",
-      flag: "/images/Italy.png",
+      flag: "/images/flag/Italy.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "كوريا",
-      flag: "/images/Kore.png",
+      flag: "/images/flag/Kore.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "الامارات",
-      flag: "/images/Dubai.png",
+      flag: "/images/flag/Dubai.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "بيرطانيا",
-      flag: "/images/England.png",
+      flag: "/images/flag/England.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "السعودية",
-      flag: "/images/Sud.png",
+      flag: "/images/flag/Sud.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "السعودية",
-      flag: "/images/Sud.png",
+      flag: "/images/flag/Sud.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "فنلند",
-      flag: "/images/Finland.png",
+      flag: "/images/flag/Finland.png",
       response: "70ms",
       color: "#46CC3F",
     },
     {
       name: "السودان",
-      flag: "/images/Suda.png",
+      flag: "/images/flag/Suda.png",
       response: "70ms",
       color: "#46CC3F",
     },
@@ -94,42 +94,40 @@ const ServerLocation = () => {
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 mt-8">
         {locations.map((location, index) => (
-          <motion.div
-            key={index}
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-              transition: { duration: 0.4, ease: "easeOut" },
-            }}
-            className="flex items-center justify-around bg-white rounded-lg shadow-md p-4 hover:shadow-xl transition-shadow"
-          >
-            <div className="">
-              <Image
-                src={location.flag}
-                alt={`${location.name} Flag`}
-                width={50}
-                height={50}
-                className="rounded-full"
-              />
-            </div>
-            <div className="">
-              <p className="text-xl font-semibold">{location.name}</p>
-              <p className="text-sm font-medium text-gray-600 flex items-center gap-2 mt-1">
-                <span
-                  className="inline-block w-2.5 h-2.5 rounded-full"
-                  style={{ backgroundColor: location.color }}
-                ></span>
-                <span
-                  className="font-semibold"
-                  style={{ color: location.color }}
-                >
-                  {location.response}
-                </span>
-                <span>الاستجابة</span>
-              </p>
-            </div>
-          </motion.div>
+         <motion.div
+  key={index}
+  initial={{ opacity: 0, y: 100 }}
+  whileInView={{
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.4, ease: "easeOut" },
+  }}
+  className="flex flex-col items-center sm:flex-row sm:justify-around bg-white rounded-lg shadow-md p-4 hover:shadow-xl transition-shadow text-center sm:text-start"
+>
+  <div className="mb-2 sm:mb-0">
+    <Image
+      src={location.flag}
+      alt={`${location.name} Flag`}
+      width={50}
+      height={50}
+      className="rounded-full"
+    />
+  </div>
+  <div>
+    <p className="text-lg sm:text-xl font-semibold">{location.name}</p>
+    <p className="text-sm font-medium text-gray-600 flex justify-center sm:justify-start items-center gap-2 mt-1">
+      <span
+        className="inline-block w-2.5 h-2.5 rounded-full"
+        style={{ backgroundColor: location.color }}
+      ></span>
+      <span className="font-semibold" style={{ color: location.color }}>
+        {location.response}
+      </span>
+      <span>الاستجابة</span>
+    </p>
+  </div>
+</motion.div>
+
         ))}
       </div>
     </div>
