9110d83277bd4382da3740f5993175d3970b70cb
Your Name <john@arabgiftcard.com>
2025-07-17 14:31:00 +0300

    add

--- app/components/Fast/BackGroundSection/BackGroundSection.jsx
+++ app/components/Fast/BackGroundSection/BackGroundSection.jsx
@@ -1,17 +1,33 @@
-import Image from "next/image"
+import Image from "next/image";
 
 const BackGroundSection = () => {
   return (
-   <div className="relative w-screen h-screen">
-  <Image
-    src="/images/Screenshot.png"
-    alt="Hero Background"
-    fill
-    className="object-cover"
-  />
-</div>
+    <div className="relative w-full h-[60vh] md:h-screen bg-white overflow-hidden flex items-center justify-center">
+      {/* المثلث الأزرق اليسار */}
+      <div
+        className="absolute top-0 left-0 w-1/4 h-[60vh] md:h-screen bg-[#0F2C4F] z-0"
+        style={{ clipPath: "polygon(0 0, 100% 50%, 0 100%)" }}
+      ></div>
 
-  )
-}
+      {/* المثلث الأزرق اليمين */}
+      <div
+        className="absolute top-0 right-0 w-1/4 h-[60vh] md:h-screen bg-[#0F2C4F] z-0"
+        style={{ clipPath: "polygon(100% 0, 0 50%, 100% 100%)" }}
+      ></div>
 
-export default BackGroundSection
+      {/* الصورة في النص */}
+      <div className="relative z-10 w-[90%] max-w-[900px] aspect-[900/907]">
+        <Image
+          src="/images/Group19550.svg"
+          alt="Center Illustration"
+          fill
+          priority
+          quality={100}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BackGroundSection;
